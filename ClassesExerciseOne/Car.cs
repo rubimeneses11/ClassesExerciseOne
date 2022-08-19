61d5177b3357ed778a667d70887ed5baa6fa66c0
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-18 23:07:05 -0500

    done

---
+++ ClassesExerciseOne/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ClassesExerciseOne
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        //Create a Make property of type string that is public
+       public string Make { get; set; }
+        //Create a Model property of type string that is public
+       public string Model { get; set; }
+        //Create a Year property of type int that is public
+       public int Year { get; set; }
+
+    }
+}
+
---
+++ ClassesExerciseOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassesExerciseOne;
+
+//Create a new instance of the Car class
+Car myCar = new Car();
+//Set values in the properties for the object
+myCar.Make = "Honda";
+myCar.Model = "Pilot";
+myCar.Year = 2017;
+//Print the values of each property to the Console
+var CarList = new List<Car>() { myCar };
+foreach (var auto in CarList)
+{
+    Console.WriteLine($"{auto.Make} {auto.Model} {auto.Year}");
+}
